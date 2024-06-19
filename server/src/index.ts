2df739d3a1d718cc68e68fe01118a76be289c8e2
legendaryyy14 <jamiecardona96@gmail.com>
2024-06-18 22:43:51 -0600

    set up MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
 // pwd: mnNzS8KDer2SlaPl
+import express, { Express } from "express";
+import mongoose from "mongoose";
+
+const app: Express = express();
+const port = process.env.PORT || 3001
+
+app.use(express.json());
+
+const mongoURI: string = "mongodb+srv://jamiecardona:mnNzS8KDer2SlaPl@personalfinancetracker.hlx5ysn.mongodb.net/"
+
+mongoose.connect(mongoURI).then(() => console.log("Connected to MongoDB!")).catch((err) => console.error("Failed to connect to MongoDB:", err))
+
+app.listen(port, () => {
+    console.log(`Server Running on Port ${port}`)
+})
