b90f72e2cbef868c4127849dc0d35dbe85796bb0
legendaryyy14 <jamiecardona96@gmail.com>
2024-06-26 16:42:59 -0600

    done with financial records api endpoints

--- server/src/routes/financial-records.ts
+++ server/src/routes/financial-records.ts
@@ -25,6 +25,35 @@ router.post("/", async (req: Request, res: Response) => {
     } catch (err) {
         res.status(500).send(err);
     }
-})
+});
+
+router.put("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const newRecordBody = req.body;
+        const record = await FinancialRecordModel.findByIdAndUpdate(
+            id,
+            newRecordBody,
+            { new: true }
+        );
+
+        if (!record) return res.status(404).send();
+
+        res.status(200).send(record)
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const record = await FinancialRecordModel.findByIdAndDelete(id);
+        if (!record) return res.status(404).send();
+        res.status(200).send(record)
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 export default router;
